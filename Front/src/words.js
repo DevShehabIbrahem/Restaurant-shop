0ff01f99492740c3d37515f39a3edcd9e2d99257
DevShehabIbrahem <shehabibrahem222@gmail.com>
2022-06-29 11:14:47 -0700

    food items

--- Front/src/words.js
+++ Front/src/words.js
@@ -9,4 +9,10 @@ export const words = {
     " cheese integrated",
     " bacon flavour",
   ],
+  itemsWords: [
+    { title: "Hamburger", img: require("./Asset/Items/1.png") },
+    { title: "Yummy Donuts", img: require("./Asset/Items/2.png") },
+    { title: "Ice Cream", img: require("./Asset/Items/3.png") },
+    { title: "Craft Pizza", img: require("./Asset/Items/4.png") },
+  ],
 };
