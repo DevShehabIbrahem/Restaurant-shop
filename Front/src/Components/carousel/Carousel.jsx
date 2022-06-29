8c2c828b58f8a670e707b6a703ff7613ade6780f
DevShehabIbrahem <shehabibrahem222@gmail.com>
2022-06-29 02:49:31 -0700

    update carousel

--- Front/src/Components/carousel/Carousel.jsx
+++ Front/src/Components/carousel/Carousel.jsx
@@ -1,24 +1,18 @@
-import React, { useRef, useState } from "react";
 import { Navigation } from "swiper";
 import { IoMdShareAlt } from "react-icons/io";
-import Bounce from "react-reveal/Bounce";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/navigation";
-
 import "swiper/css";
 import "../../css/carousel/Carousel.css";
-import image1 from "../../Asset/SliderCarousel/sandw1.png";
-import image2 from "../../Asset/SliderCarousel/shape.png";
-import image3 from "../../Asset/SliderCarousel/Sandw.png";
-import image4 from "../../Asset/SliderCarousel/sandow3.png";
+
 import burger from "../../Asset/burger.png";
 import bg from "../../Asset/carousel.jpg";
 import { words } from "../../words";
+import { images } from "../../Asset";
 
 const Carousel = () => {
-  const images = [image1, image2, image3, image4];
-  const bgc = [bg, bg, bg, bg];
+  const { listOfCarousel } = words;
 
   return (
     <>
@@ -40,26 +34,17 @@ const Carousel = () => {
                   <h1>{words.update}</h1>
                   <h1>{words.chicken}</h1>
                   <ul>
-                    <li>
-                      <span>
-                        <IoMdShareAlt />
-                      </span>
-                      integrated with mushroom
-                    </li>
-                    <li>
-                      <span>
-                        <IoMdShareAlt />
-                      </span>
-                      cheese integrated
-                    </li>
-                    <li>
-                      <span>
-                        <IoMdShareAlt />
-                      </span>
-                      bacon flavour
-                    </li>
+                    {listOfCarousel.map((list) => (
+                      <li>
+                        <span>
+                          <IoMdShareAlt />
+                        </span>
+                        {list}
+                      </li>
+                    ))}
                   </ul>
                 </div>
+
                 <div className=" mb-10 w-[68%] lg:w-[50%]">
                   <img src={img} alt="carousel" />
                 </div>
@@ -68,17 +53,6 @@ const Carousel = () => {
           ))}
         </Swiper>
       </div>
-
-      {/*{images.map((img) => (
-          <SwiperSlide className="flex justify-center items-center h-[10px]">
-            <div className="relative">
-              <img src={bg} alt="bg" />
-            </div>
-            <div className="absolute top-0">
-              <img src={img} alt="carousel" className="swiper-slide" />
-            </div>
-          </SwiperSlide>
-        ))}*/}
     </>
   );
 };
