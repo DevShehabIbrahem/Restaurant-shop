4adfe820e34fe4ef45301bee365af91733a4823b
DevShehabIbrahem <shehabibrahem222@gmail.com>
2022-06-30 00:01:02 -0700

    welcome

--- Front/src/Components/index.js
+++ Front/src/Components/index.js
@@ -1,2 +1,3 @@
 export { default as Items } from "./Items/Items";
 export { default as Carousel } from "./carousel/Carousel.jsx";
+export { default as WelcomeSection } from "./Welcome/WelcomeSection.jsx";
